98326c847fcd2ac66c516acbbfba4b2685fb8a31
almac2022 <al@newgraphenvironment.com>
2025-03-11 08:42:43 -0700

    correct col header name and clean up docs

--- app.R
+++ app.R
@@ -3,15 +3,6 @@ library(bslib)
 library(DT)
 # library(readr)
 
-
-# looks like the export from zotero is now breaking our xciter::xct_keys_to_inline_table_col function. not sure why - added last
-# citation by hand.... Need to look into it
-# httr::GET(
-#   url = "https://raw.githubusercontent.com/NewGraphEnvironment/new_graphiti/main/assets/NewGraphEnvironment.bib",
-#   httr::write_disk("references.bib", overwrite = TRUE)
-# )
-
-
 # # use the csv vs the tribble
 # data_raw <- readr::read_csv(
 #
@@ -34,7 +25,7 @@ library(DT)
 #   readr::write_csv(
 #     file = 'recommendations.csv'
 #   )
-
+#
 data_clean <- readr::read_csv('recommendations.csv')
 
 
@@ -86,7 +77,7 @@ server <- function(input, output, session) {
               $(td).css('cursor', 'pointer');
               $(td).css('font-weight', 'bold');
               if(cellData === null || cellData === '' || cellData === 'NA') {
-                $(td).html('Click to rate!');
+                $(td).html('Click to add input!');
                 $(td).css('color', '#666');
                 $(td).css('font-style', 'italic');
               }
